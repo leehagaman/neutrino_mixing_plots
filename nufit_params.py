4bfe826897d2a94456af897d7a2725190d1362d6
leehagaman <62082849+leehagaman@users.noreply.github.com>
2025-02-27 16:22:51 -0600

    added neutrino mass correlation plot

---
+++ nufit_params.py
@@ -0,0 +1,54 @@
+
+import numpy as np
+
+# from NuFit 6.0, http://www.nu-fit.org/?q=node/294
+
+# degrees
+theta12 = 33.68
+theta12_err = 0.5*(0.73+0.70)
+theta23 = 43.3
+theta23_err = 0.5*(1.0+0.8)
+theta13 = 8.56
+theta13_err = 0.5*(0.11+0.11)
+deltaCP = 212
+deltaCP_err = 0.5*(26+41)
+# eV^2
+delta2_m21 = 7.49e-5
+delta2_m21_err = 0.5*(0.19+0.19)*1e-5
+delta2_m3l = 2.513e-3
+delta2_m3l_err = 0.5*(0.021+0.019)*1e-3
+
+# inverted ordering
+
+# degrees
+theta12_inv = 33.68
+theta12_inv_err = 0.5*(0.73+0.70)
+theta23_inv = 47.9
+theta23_inv_err = 0.5*(0.7+0.9)
+theta13_inv = 8.59
+theta13_inv_err = 0.5*(0.11+0.11)
+deltaCP_inv = 274
+deltaCP_inv_err = 0.5*(22+25)
+# eV^2
+delta2_m21_inv = 7.49e-5
+delta2_m21_inv_err = 0.5*(0.19+0.19)*1e-5
+delta2_m3l_inv = -2.484e-3
+delta2_m3l_inv_err = 0.5*(0.020+0.020)*1e-3
+
+theta12 = np.radians(theta12)
+theta12_err = np.radians(theta12_err)
+theta23 = np.radians(theta23)
+theta23_err = np.radians(theta23_err)
+theta13 = np.radians(theta13)
+theta13_err = np.radians(theta13_err)
+deltaCP = np.radians(deltaCP)
+deltaCP_err = np.radians(deltaCP_err)
+
+theta12_inv = np.radians(theta12_inv)
+theta12_inv_err = np.radians(theta12_inv_err)
+theta23_inv = np.radians(theta23_inv)
+theta23_inv_err = np.radians(theta23_inv_err)
+theta13_inv = np.radians(theta13_inv)
+theta13_inv_err = np.radians(theta13_inv_err)
+deltaCP_inv = np.radians(deltaCP_inv)
+deltaCP_inv_err = np.radians(deltaCP_inv_err)
